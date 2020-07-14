ddad3bb87c445e84ae6a5c93ed4b18fdb5021a28
Marie Foxall <marie@wastedeffort.ca>
2020-07-14 15:57:58 -0400

    3

--- data/words.js
+++ data/words.js
@@ -1,8 +1,63 @@
 const words = [
   {
-    id: '1000',
-    word: 'bacon',
-    letterCount: '5',
+    id: "1000",
+    word: "bacon",
+    letterCount: "5",
+  },
+  {
+    id: "1001",
+    word: "rutabaga",
+    letterCount: "8",
+  },
+  {
+    id: "1002",
+    word: "synecdoche",
+    letterCount: "10",
+  },
+  {
+    id: "1003",
+    word: "favourite",
+    letterCount: "9",
+  },
+  {
+    id: "1004",
+    word: "bungalow",
+    letterCount: "8",
+  },
+  {
+    id: "1005",
+    word: "asphalt",
+    letterCount: "7",
+  },
+  {
+    id: "1006",
+    word: "bismuth",
+    letterCount: "7",
+  },
+  {
+    id: "1007",
+    word: "misanthrope",
+    letterCount: "11",
+  },
+  {
+    id: "1008",
+    word: "schadenfreude",
+    letterCount: "13",
+  },
+  {
+    id: "1009",
+    word: "cubic",
+    letterCount: "5",
+  },
+  {
+    id: "1010",
+    word: "mezzanine",
+    letterCount: "9",
+  },
+  {
+    id: "1011",
+    word: "finally",
+    letterCount: "7",
   },
 ];
 
--- server.js
+++ server.js
@@ -4,8 +4,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const { clients } = require("./data/clients");
+const { words } = require("./data/words");
+
 const { v4: uuidv4 } = require("uuid");
-console.log(clients);
 
 express()
   .use(function (req, res, next) {
@@ -52,6 +53,36 @@ express()
     );
     clients.splice(deleteClientIndex, 1);
     res.status(200).send(clients);
+  })
+
+  .get("/hangman/word/:id", (req, res) => {
+    const wordID = req.params.id;
+    const thisWord = words.find((word) => word.id === wordID);
+    res.status(200).json(thisWord.word);
+  })
+
+  .get("/hangman/word/", (req, res) => {
+    const randomWordIndex = Math.floor(Math.random() * 11);
+    console.log(randomWordIndex);
+    const secretWord = words[randomWordIndex];
+    console.log(secretWord);
+    res.status(200).json({
+      id: secretWord.id,
+      letterCount: secretWord.letterCount,
+    });
+  })
+
+  .get("/hangman/guess/:id/:letter", (req, res) => {
+    const guessWordID = req.params.id;
+    const guessLetter = req.params.letter;
+    const guessWordObject = words.find((word) => word.id === guessWordID);
+    const guessWord = guessWordObject.word;
+    const guessWordArray = guessWord.split("");
+    const guessArray = guessWordArray.map((letter) => letter === guessLetter);
+    res.status(200).json({
+      status: 200,
+      guess: guessArray,
+    });
   })
 
   .listen(8000, () => console.log(`Listening on port 8000`));
